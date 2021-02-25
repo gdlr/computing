f113430231f5f544a4aa6c24c626e9c1c2102b7e
Gabriel De La Rosa <gabrieldelarosa@Gabriels-MacBook-Pro.local>
2021-02-24 18:03:57 -0800

    assignment_2b

--- R/power_gen.R
+++ R/power_gen.R
@@ -1,21 +1,13 @@
-# Make a function!
-##
-## Parameters
-##
-# g = gravity,
-# p_air = density of air
-# crolling = rolling drag coefficient
-# cdrag = aerodynamic drag coefficient
-##
-## Inputs
-##
-# m = car mass
-# V = car speed
-# A = surface area
 
 power_gen <- function(m, V, A, g = 9.8, p_air = 1.2, crolling = 0.015, cdrag = 0.3){
-  
+  # Body 
   Pb <- crolling * m * g * V + 0.5 * A * p_air * cdrag * V^3
-  
+  #Error checking:
+  V = ifelse((V < 0), return("vehicle speed must be positive"), V)
+  m = ifelse((m < 0), return("vehicle mass cannot be negative"), m)
+  g = ifelse((g != 9.8), return("vehicle is not on Earth?"), g)
+  # Output is the power output
   return(Pb)
 }
+
+
